51cb16e18c75435e23038ff395cc0cafc1b2310b
Daniel Montanez <danielmsmalaga@gmail.com>
2020-10-25 10:20:28 +0100

    Implementación inicial AbstractDAO

--- common/Constants.java
+++ common/Constants.java
@@ -1,5 +1,8 @@
 package common;
 
 public class Constants {
-
+	public static final String CONTROLLER = "com.mysql.jdbc.Driver";
+	public static final String URL = "jdbc:mysql://localhost:3306/taller?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public static final String USER = "root";
+	public static final String PASSWORD = "1234";
 }
--- dao/AbstractDAO.java
+++ dao/AbstractDAO.java
@@ -1,5 +1,32 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import common.Constants;
+
 public abstract class AbstractDAO {
 
+	protected Connection con;
+	
+	public AbstractDAO() {
+		connectDB();
+	}
+	
+	/**
+	 * Conecta con la base de datos
+	 */
+	private void connectDB() {
+		try {
+            Class.forName(Constants.CONTROLLER);
+            try {
+				this.con = DriverManager.getConnection(Constants.URL, Constants.USER, Constants.PASSWORD);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+	}
+	
 }
