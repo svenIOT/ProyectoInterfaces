27b65a883140c239cbdeb5f14d88da639e918604
andrestorres04 <61821537+andrestorres04@users.noreply.github.com>
2020-11-13 13:51:28 +0100

    Añadido SELECT_PROPOSITION

--- common/Constants.java
+++ common/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String CONTROLLER = "com.mysql.cj.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/taller?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	public static final String USER = "root";
-	public static final String PASSWORD = "1234";
+	public static final String PASSWORD = "123456";
 	// ----------------
 
 	// CONSULTAS-------
@@ -16,6 +16,8 @@ public class Constants {
 	public static final String SELECT_CARS = "SELECT vehiculo.num_bastidor, coche.mat_coche, vehiculo.marca, vehiculo.modelo, vehiculo.combustible, vehiculo.precio FROM taller.vehiculo INNER JOIN taller.coche ON vehiculo.num_bastidor = coche.num_bastidor;";
 	public static final String SELECT_MOTORCICLES = "SELECT vehiculo.num_bastidor, motocicleta.mat_moto, vehiculo.marca, vehiculo.modelo, vehiculo.combustible, vehiculo.precio FROM taller.vehiculo INNER JOIN taller.motocicleta ON vehiculo.num_bastidor = motocicleta.num_bastidor;";
 	public static final String SELECT_MOPEDS = "SELECT vehiculo.num_bastidor, ciclomotor.mat_ciclo, vehiculo.marca, vehiculo.modelo, vehiculo.combustible, vehiculo.precio FROM taller.vehiculo INNER JOIN taller.ciclomotor ON vehiculo.num_bastidor = ciclomotor.num_bastidor;";
+	//SellingPropositionDAO
+	public static final String SELECT_PROPOSITION = "SELECT * FROM taller.propuesta";
 	// ----------------
 
 }
