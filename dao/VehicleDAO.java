0f5450847f9aef18df5a2acfc09b7220fcb997b7
andrestorres04 <61821537+andrestorres04@users.noreply.github.com>
2020-11-22 13:12:53 +0100

    Add files via upload

--- dao/VehicleDAO.java
+++ dao/VehicleDAO.java
@@ -19,46 +19,38 @@ public class VehicleDAO extends AbstractDAO {
 	/**
 	 * Comprueba el tipo de vehiculo y lo añade a la BBDD
 	 * 
-	 * @param v          objeto vehiculo
-	 * @param enrollment matrícula del vehículo en cuestión, viene de un txtField
+	 * @param v           objeto vehiculo
+	 * @param vehicleType tipo de vehículo que viene de un combobox
+	 * @param enrollment  matrícula del vehículo en cuestión, viene de un txtField
 	 */
-	public void addVehicle(Vehicle v, String enrollment, boolean isSaleVehicle) {
+	public void addVehicle(Vehicle v, String vehicleType, String enrollment) {
 		String vehicleTypeInsert;
 		// Asignar tipo de vehiculo para insertarlo en su tabla
-		if (v.getTipoVehiculo().equalsIgnoreCase("coche")) {
+		if (vehicleType.equalsIgnoreCase("coche")) {
 			vehicleTypeInsert = "INSERT INTO taller.coche (mat_coche, num_bastidor) VALUES ('" + enrollment + "', '"
 					+ v.getNum_bastidor() + "')";
-		} else if (v.getTipoVehiculo().equalsIgnoreCase("motocicleta")) {
+		} else if (vehicleType.equalsIgnoreCase("motocicleta")) {
 			vehicleTypeInsert = "INSERT INTO taller.motocicleta (mat_moto, num_bastidor) VALUES ('" + enrollment
 					+ "', '" + v.getNum_bastidor() + "')";
 		} else {
 			vehicleTypeInsert = "INSERT INTO taller.ciclomotor (mat_ciclo, num_bastidor) VALUES ('" + enrollment
 					+ "', '" + v.getNum_bastidor() + "' )";
 		}
-		
-		// Comprobar si es un vehículo a la venta o un vehículo a reparar (evitar error SQL)
-		var insert = "";
-		
-		if(isSaleVehicle) {
-			insert = "INSERT INTO taller.vehiculo (num_bastidor, cod_ventas, cod_cliente, cod_conce, tipo_vehiculo, marca, modelo, combustible, precio) VALUES ("
-					+ "'" + v.getNum_bastidor() + "', " + v.getCod_ventas() + ", " + v.getCod_cliente() + ", "
-					+ v.getCod_conce() + ", '" + v.getTipoVehiculo() + "', '" + v.getMarca() + "', '"
-					+ v.getModelo() + "', '" + v.getCombustible() + "', " + v.getPrecio() + ");";
-		} else {
-			insert = "INSERT INTO taller.vehiculo (num_bastidor, cod_ventas, cod_cliente, cod_conce, tipo_vehiculo, marca, modelo, combustible, precio) VALUES ("
-					+ "'" + v.getNum_bastidor() + "', NULL, NULL, "+ v.getCod_conce() + ", '" + v.getTipoVehiculo() + "', '" + v.getMarca() + "', '"
-					+ v.getModelo() + "', '" + v.getCombustible() + "', " + v.getPrecio() + ");";
-		}
-		
+
 		try {
 			stm = con.createStatement();
-			stm.executeUpdate(insert);
+			stm.executeUpdate(
+					"INSERT INTO taller.vehiculo (num_bastidor, cod_ventas, cod_cliente, cod_conce, marca, modelo, combustible, precio) VALUES ("
+							+ "'" + v.getNum_bastidor() + "', " + v.getCod_ventas() + ", " + v.getCod_cliente() + ", "
+							+ v.getCod_conce() + ", '" + v.getMarca() + "', '" + v.getModelo() + "', '"
+							+ v.getCombustible() + "', " + v.getPrecio() + ")");
 			stm.executeUpdate(vehicleTypeInsert);
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 	}
 
+	
 	/**
 	 * Devuelve una lista con todos los coches
 	 * 
@@ -70,10 +62,8 @@ public class VehicleDAO extends AbstractDAO {
 			stm = con.createStatement();
 			rs = stm.executeQuery(Constants.SELECT_CARS_AlL_DATA);
 			while (rs.next()) {
-				cars.add(new Car(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"),
-						rs.getString("combustible"), rs.getString("precio"), rs.getInt("cod_ventas"),
-						rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo"),
-						rs.getString("mat_coche")));
+				cars.add(new Car(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"), rs.getString("combustible"), rs.getString("precio"), rs.getInt("cod_ventas"),
+						rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo"), rs.getString("mat_coche")));
 			}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
@@ -92,10 +82,8 @@ public class VehicleDAO extends AbstractDAO {
 			stm = con.createStatement();
 			rs = stm.executeQuery(Constants.SELECT_MOTORCICLES_ALL_DATA);
 			while (rs.next()) {
-				motorcycles.add(new Motorcycle(rs.getString("num_bastidor"), rs.getString("marca"),
-						rs.getString("modelo"), rs.getString("combustible"), rs.getString("precio"),
-						rs.getInt("cod_ventas"), rs.getInt("cod_cliente"), rs.getInt("cod_conce"),
-						rs.getString("tipo_vehiculo"), rs.getString("mat_moto")));
+				motorcycles.add(new Motorcycle(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"), rs.getString("combustible"), rs.getString("precio"), rs.getInt("cod_ventas"),
+						rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo"), rs.getString("mat_moto")));
 			}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
@@ -115,17 +103,15 @@ public class VehicleDAO extends AbstractDAO {
 			stm = con.createStatement();
 			rs = stm.executeQuery(Constants.SELECT_MOPEDS_ALL_DATA);
 			while (rs.next()) {
-				moped.add(new Moped(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"),
-						rs.getString("combustible"), rs.getString("precio"), rs.getInt("cod_ventas"),
-						rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo"),
-						rs.getString("mat_ciclo")));
+				moped.add(new Moped(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"), rs.getString("combustible"), rs.getString("precio"), rs.getInt("cod_ventas"),
+						rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo"), rs.getString("mat_ciclo")));
 			}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 		return moped;
 	}
-
+	
 	/**
 	 * Devuelve una lista con todos los vehículos
 	 * 
@@ -137,9 +123,8 @@ public class VehicleDAO extends AbstractDAO {
 			stm = con.createStatement();
 			rs = stm.executeQuery(Constants.SELECT_VEHICLES);
 			while (rs.next()) {
-				vehicles.add(new Vehicle(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"),
-						rs.getString("combustible"), rs.getString("precio"), rs.getInt("cod_ventas"),
-						rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo")));
+				vehicles.add(new Vehicle(rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"), rs.getString("combustible"), 
+						rs.getString("precio"),rs.getInt("cod_ventas"), rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo")));
 			}
 		} catch (SQLException ex) {
 			ex.printStackTrace();
@@ -168,8 +153,8 @@ public class VehicleDAO extends AbstractDAO {
 		}
 		return vehicle;
 	}
-
-	public Integer getVehiclesUnsold() {
+	
+	public Integer getCountVehiclesUnsold() {
 		int vehiclesUnsold = 0;
 		try {
 			stm = con.createStatement();
@@ -182,5 +167,24 @@ public class VehicleDAO extends AbstractDAO {
 		}
 		return vehiclesUnsold;
 	}
+
+	public List<Vehicle> getVehiclesUnsold() {
+		var vehicle = new ArrayList<Vehicle>();
+		try {
+			stm = con.createStatement();
+			rs = stm.executeQuery(Constants.SELECT_VEHICLES_UNSOLD);
+			while (rs.next()) {
+				vehicle.add(new Vehicle (rs.getString("num_bastidor"), rs.getString("marca"), rs.getString("modelo"), rs.getString("combustible"), 
+						rs.getString("precio"),rs.getInt("cod_ventas"), rs.getInt("cod_cliente"), rs.getInt("cod_conce"), rs.getString("tipo_vehiculo")));
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return vehicle;
+	
+	}
+	
+
+
 
 }
